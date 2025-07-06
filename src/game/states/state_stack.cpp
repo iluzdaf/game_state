02e5e455349f0f412383e7f98afdafc8d904f6fd
Fadzuli <fadzuli.said@gmail.com>
2025-07-06 22:12:35 +0800

    Add implementation of state stack

--- src/game/states/state_stack.cpp
+++ src/game/states/state_stack.cpp
@@ -3,16 +3,38 @@
 
 void StateStack::push(std::unique_ptr<GameState> state)
 {
-    stack.clear();
+    if (!state)
+        throw std::runtime_error("StateStack: push received nullptr GameState");
+
+    if (!stack.empty())
+        top().onPause();
+
     stack.push_back(std::move(state));
+
+    top().onEnter();
 }
 
 void StateStack::pop()
 {
+    if (!stack.empty())
+    {
+        top().onExit();
+
+        stack.pop_back();
+
+        if (!stack.empty())
+            top().onResume();
+    }
 }
 
 void StateStack::replace(std::unique_ptr<GameState> state)
 {
+    if (!state)
+        throw std::runtime_error("StateStack: replace received nullptr GameState");
+
+    pop();
+
+    push(std::move(state));
 }
 
 void StateStack::update(float deltaTime)
